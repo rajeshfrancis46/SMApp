f74b7c8ceeeb6636ab7e9bb119ff4e8eeeee9665
rajeshfrancis <rajeshfrancis@gmail.com>
2023-07-10 20:18:07 +0530

    session 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,22 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-/* // Learn more about configurDaing Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); */
+builder.Services.AddCors();
+
+// Build part
 
 var app = builder.Build();
 
-/* // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization(); */
+/* // Configure the HTTP request pipeline.*/
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
