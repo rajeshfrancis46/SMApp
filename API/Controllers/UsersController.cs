101d3560878597628ac26032868c8b02b4029329
rajeshfrancis <rajeshfrancis@gmail.com>
2023-07-14 13:47:59 +0530

    sect 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   [Authorize]
+   
     public class UsersController:BaseApiController
     {
         private readonly DataContext _context;
@@ -22,6 +22,7 @@ namespace API.Controllers
             var users= await _context.Users.ToListAsync();
             return users;
          }
+         [Authorize]
          [HttpGet("{id}")]
          public async Task<ActionResult<AppUser>> GetUser(int id)
          {
